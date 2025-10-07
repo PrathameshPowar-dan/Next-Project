22fb75a7326161b7d77f18781b68a50b9599ef22
PrathameshPowar-dan <powarprathamesh579@gmail.com>
2025-10-07 20:50:46 +0530

    Reuseable Component and Trading View Integration

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,19 @@
 import Header from "@/components/Header";
+import ChartWidget from "@/components/ChartWidget";
+import { MARKET_OVERVIEW_WIDGET_CONFIG } from "@/lib/constants";
+
 export default function Home() {
   return (
     <>
       <Header />
-      <div className="flex justify-center items-center h-screen">
-      </div>
+      <section className="flex justify-center items-center h-screen">
+        <ChartWidget
+        title="Market Overview"
+        scriptUrl="https://s3.tradingview.com/external-embedding/embed-widget-market-overview.js"
+        config={MARKET_OVERVIEW_WIDGET_CONFIG}
+        className="custom-chart"
+        />
+      </section>
     </>
   );
 }
